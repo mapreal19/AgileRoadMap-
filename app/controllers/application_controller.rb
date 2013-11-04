45d5453921cc4aeb60c36b6d0e7abd9e005ca5f3
Mario Perez <mapreal19@gmail.com>
2013-11-04 13:02:17 +0100

    Multilingual setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,9 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   private
+  	# http://xyzpub.com/en/ruby-on-rails/3.2/i18n_mehrsprachige_rails_applikation.html
   	def set_locale
   		I18n.locale = params[:locale] || I18n.default_locale
+  		Rails.application.routes.default_url_options[:locale]= I18n.locale 
   	end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+	
+	def lang_link(link_text, lang)
+		class_name = I18n.locale == lang ? 'active' : ''
+		#TODO stay in the same route
+		content_tag(:li, class: class_name) do 
+			link_to url_for(action: 'home', locale: lang) do
+				if class_name == 'active'
+					(link_text + ' ' + content_tag(:i, "", class: 'icon-ok')).html_safe
+				else
+					link_text
+				end
+			end
+		end
+	end
+
 end
