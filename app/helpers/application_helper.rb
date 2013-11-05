f1dd115a4ba679c0e8775cb973005c48f8142272
Mario Perez <mapreal19@gmail.com>
2013-11-05 17:46:20 +0100

    language menu fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
 		class_name = I18n.locale == lang ? 'active' : ''
 		#TODO stay in the same route
 		content_tag(:li, class: class_name) do 
-			link_to url_for(action: 'home', locale: lang) do
+			link_to url_for(locale: lang) do
 				if class_name == 'active'
 					(link_text + ' ' + content_tag(:i, "", class: 'icon-ok')).html_safe
 				else
