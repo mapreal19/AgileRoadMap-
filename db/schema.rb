fccc941f2696545c5f591966a25ad6828e8f748d
Mario Perez <mapreal19@gmail.com>
2013-11-07 20:28:32 +0100

    Made a basic User model (with secure passwords)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131107184140) do
+ActiveRecord::Schema.define(version: 20131107185231) do
 
   create_table "practicas", force: true do |t|
     t.integer  "position"
@@ -28,5 +28,7 @@ ActiveRecord::Schema.define(version: 20131107184140) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
