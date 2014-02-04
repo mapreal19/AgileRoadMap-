01b454b65b504774c8936e600d5d1112b3ea76ad
Mario Perez <mapreal19@gmail.com>
2014-02-04 18:31:54 +0100

    Columna no_aplicable añadida a user_practicas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140204162544) do
+ActiveRecord::Schema.define(version: 20140204172209) do
 
   create_table "contribucions", force: true do |t|
     t.integer  "practica_id"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20140204162544) do
     t.text     "comment"
     t.integer  "effort"
     t.integer  "range"
+    t.boolean  "no_aplicable",    default: false
   end
 
   create_table "users", force: true do |t|
