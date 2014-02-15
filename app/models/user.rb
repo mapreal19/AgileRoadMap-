9eb7c6d33c0803fa5a462a994223056e20d11d38
Mario Perez <mapreal19@gmail.com>
2014-02-15 12:00:13 +0100

    Funcion clone_objetivos

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
 	before_create :create_remember_token
 
 	has_many :user_practicas, -> { order("position") }, dependent: :destroy
+	has_many :user_objetivos, -> { order("position") }, dependent: :destroy
+
 
 	validates :name, presence: true, length: { maximum: 50 }
 
@@ -31,6 +33,19 @@ class User < ActiveRecord::Base
 				legacy_position: practica.position,
 				effort: practica.effort,
 				range: -1).save
+		end
+	end
+
+	def clone_objetivos
+		all_objetivos = Objetivo.all
+
+		index = 0
+		all_objetivos.each do |objetivo|
+			self.user_objetivos.build(
+				objetivo_id: objetivo.id, 
+				position: index
+				).save
+			index += 1
 		end
 	end
 
