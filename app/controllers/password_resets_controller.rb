a406d165b83f7a35487451ad111e9ac2f094ac35
Mario Perez <mapreal19@gmail.com>
2014-02-18 12:25:18 +0100

    create action in password_resets_controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,18 @@
+# encoding: utf-8
+
 class PasswordResetsController < ApplicationController
   def new
   end
+
+  def create
+  	user = User.find_by(email: password_reset_params[:email])
+  	user.send_password_reset if user
+  	redirect_to root_url, :notice => "Email envidado con instrucciones para restablecer la contraseña"
+	end
+
+	private
+  	def password_reset_params
+      params.permit(:email)
+    end
+
 end
