608e7e5c4f036a6ce43df5c231456745be719f37
Mario Perez <mapreal19@gmail.com>
2014-02-19 18:18:41 +0100

    Bug fix ajax calls

--- app/controllers/user_practicas_controller.rb
+++ app/controllers/user_practicas_controller.rb
@@ -2,30 +2,37 @@ class UserPracticasController < ApplicationController
 
 	def sort
 		@user_practica = UserPractica.find(params[:id])
-		@user_practica.insert_at(params[:user_practica][:row_order_position].to_i + 1)
-    # this action will be called via ajax
+    if current_user?(@user_practica.user)
+		  @user_practica.insert_at(params[:user_practica][:row_order_position].to_i + 1)
+    end
     render nothing: true
   end
 
   def update_range
   	@user_practica = UserPractica.find(params[:id])
-  	@user_practica.range = params[:user_practica][:range].to_i
-  	@user_practica.save
+    if current_user?(@user_practica.user)
+    	@user_practica.range = params[:user_practica][:range].to_i
+    	@user_practica.save
+    end
   	render nothing: true
   end
 
   def update_comment
     @user_practica = UserPractica.find(params[:id])
-    @user_practica.comment = params[:user_practica][:comment]
-    @user_practica.save
+    if current_user?(@user_practica.user)
+      @user_practica.comment = params[:user_practica][:comment]
+      @user_practica.save
+    end
     render nothing: true
   end
 
   def update_aplicable
     @user_practica = UserPractica.find(params[:id])
-    @user_practica.no_aplicable = params[:user_practica][:no_aplicable]
-    @user_practica.save
-    logger.info "UserPractica attributes hash: #{@user_practica.attributes.inspect}"
+    if current_user?(@user_practica.user)
+      @user_practica.no_aplicable = params[:user_practica][:no_aplicable]
+      @user_practica.save
+      #logger.info "UserPractica attributes hash: #{@user_practica.attributes.inspect}"
+    end
     render nothing: true
   end
 
