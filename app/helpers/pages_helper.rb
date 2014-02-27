86ecce085f76a5c98e2057d829c5674cb564186b
Mario Perez <mapreal19@gmail.com>
2014-02-27 18:06:52 +0100

    helper for active class

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,9 @@
 module PagesHelper
+  def active_class(action, controller)
+    if params[:action] == action and params[:controller] == controller
+      'active'
+    else
+      ''
+    end
+  end
 end
