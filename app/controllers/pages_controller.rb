24f3926a3e3c6f9a08dc4bc817151156e8ba35ad
Mario Perez <mapreal19@gmail.com>
2014-04-25 18:27:12 +0200

    User Ambito pie chart

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -47,6 +47,17 @@ class PagesController < ApplicationController
       # inject(:+) suma todos los elementos del array. to_f para que la division no sea entera.
       @pracs_position[key] = array.inject(:+).to_f / array.size
     end
+
+    # --- Users
+    @user_ambitos = User.group(:ambito_trabajo_id).count
+
+    mappings = AmbitoTrabajo.all.pluck(:id, :nombre)
+    hash_mappings = {}
+    mappings.each do |mapping|
+      hash_mappings[mapping[0]] = mapping[1]
+    end
+
+    @user_ambitos = Hash[@user_ambitos.map {|k, v| [hash_mappings[k], v] }]
   end
 
 end
