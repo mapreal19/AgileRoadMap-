e5161e0dd6e3f569cc8dda59e7d239f77f9f288d
Mario Perez <mapreal19@gmail.com>
2014-04-25 18:01:34 +0200

    User Pracs stats

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -34,6 +34,19 @@ class PagesController < ApplicationController
       # inject(:+) suma todos los elementos del array. to_f para que la division no sea entera.
       @result[key] = array.inject(:+).to_f / array.size
     end
+
+    # --- Practicas
+
+    @pracs_position = {}
+    UserPractica.all.each do |user_practica|
+      @pracs_position[user_practica.practica_id] ||= []
+      @pracs_position[user_practica.practica_id].push user_practica.position
+    end
+
+    @pracs_position.each do |key, array|
+      # inject(:+) suma todos los elementos del array. to_f para que la division no sea entera.
+      @pracs_position[key] = array.inject(:+).to_f / array.size
+    end
   end
 
 end
