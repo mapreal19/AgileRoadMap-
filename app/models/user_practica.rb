6f6303dc45ed12474b22a888764167dbfde62ae5
Mario Perez <mapreal19@gmail.com>
2014-04-26 14:20:03 +0200

    Bar chart margen de mejora

--- app/models/user_practica.rb
+++ app/models/user_practica.rb
@@ -47,4 +47,20 @@ class UserPractica < ActiveRecord::Base
 
     result
   end
+
+  def self.get_margen_stats
+    result = {}
+    UserPractica.all.each do |user_practica|
+      result[user_practica.practica_id] ||= []
+      # No tenemos en cuenta si range == -1 (No definido)
+      result[user_practica.practica_id].push user_practica.range if user_practica.range >= 0
+    end
+
+    result.each do |key, array|
+      # inject(:+) suma todos los elementos del array. to_f para que la division no sea entera.
+      result[key] = array.inject(:+).to_f / array.size
+    end
+
+    result
+  end
 end
