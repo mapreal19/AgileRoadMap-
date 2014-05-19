dc206f5c8ece90d1104a88f7bafb3e23c10acc95
Mario Perez <mapreal19@gmail.com>
2014-05-19 15:32:29 +0200

    modified steps

--- features/step_definitions/cucumber_steps.rb
+++ features/step_definitions/cucumber_steps.rb
@@ -2,10 +2,8 @@
 
 Given /^I have signed up as "(.*?)"$/ do |email|
   visit('/es/signin')
-  save_and_open_page
   fill_in('Email', :with => email)
   fill_in('Contraseña', :with => 's3cr3t')
-  save_and_open_page
   click_button('Login')
 end
 
