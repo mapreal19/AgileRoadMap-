daa17d0fd19e89c0a005b56484e70139fdd33ff8
Mario Perez <mapreal19@gmail.com>
2014-06-05 11:21:00 +0200

    cucumber scenario

--- features/login.feature
+++ features/login.feature
@@ -1,4 +1,7 @@
 Feature: login system
 
   Scenario: Signing in
-    Given I have signed up as "user1@gmail.com"
+  	Given a user with email "user1@gmail.com" and password "secret"
+    When I login as "user1@gmail.com" with password "secret"
+    Then I should see h2 "Roadmap de Prácticas Ágiles"
+    # Then I should see
--- features/step_definitions/cucumber_steps.rb
+++ features/step_definitions/cucumber_steps.rb
@@ -1,10 +1,19 @@
 # encoding: utf-8
 
-Given /^I have signed up as "(.*?)"$/ do |email|
+Given(/^a user with email "(.*?)" and password "(.*?)"$/) do |email, password|
+	@user = User.new(email: email, password: password, password_confirmation: password)  
+	@user.save(validate: false)
+end
+
+When /^I login as "(.*?)" with password "(.*?)"$/ do |email, password|
   visit('/es/signin')
   fill_in('Email', :with => email)
-  fill_in('Contraseña', :with => 's3cr3t')
+  fill_in('Contraseña', :with => password)
   click_button('Login')
+end
+
+Then(/^I should see h2 "(.*?)"$/) do |text|
+	find("h2", text: text)
 end
 
 Then /^show me the page$/ do
