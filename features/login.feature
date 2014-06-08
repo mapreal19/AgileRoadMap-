24d2bdceb7a5caaad181641e0533a0a45767bca3
Mario Perez <mapreal19@gmail.com>
2014-06-08 11:41:59 +0200

    Test login succesful

--- features/login.feature
+++ features/login.feature
@@ -4,3 +4,4 @@ Feature: login system
   	Given a user with email "user1@gmail.com" and password "secret"
     When I login as "user1@gmail.com" with password "secret"
     Then I should see h2 "Roadmap de Prácticas Ágiles"
+    And the user "user1@gmail.com" should have 42 pracs
--- features/step_definitions/cucumber_steps.rb
+++ features/step_definitions/cucumber_steps.rb
@@ -1,8 +1,13 @@
 # encoding: utf-8
 
+
+# Need to rake db:migrate RAILS_ENV=test and rake db:seed RAILS_ENV=test
+
 Given(/^a user with email "(.*?)" and password "(.*?)"$/) do |email, password|
 	@user = User.new(email: email, password: password, password_confirmation: password)  
 	@user.save(validate: false)
+	@user.clone_practicas
+  @user.clone_objetivos
 end
 
 When /^I login as "(.*?)" with password "(.*?)"$/ do |email, password|
@@ -15,6 +20,13 @@ end
 Then(/^I should see h2 "(.*?)"$/) do |text|
 	find("h2", text: text)
 end
+
+Then(/^the user "(.*?)" should have (\d+) pracs$/) do |user_email, num_pracs|
+  the_user = User.find_by(email: user_email)
+  expect(the_user.user_practicas.count).to eq(42)
+  #@user.user_practicas.count.should == 42
+end
+
 
 Then /^show me the page$/ do
   save_and_open_page
