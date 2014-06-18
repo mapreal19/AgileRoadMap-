1df95deab3a5ba474c4370eaacdbdd494c357582
Mario Perez <mapreal19@gmail.com>
2014-06-18 18:52:09 +0200

    added test for comment objetivo

--- features/step_definitions/user_practicas_steps.rb
+++ features/step_definitions/user_practicas_steps.rb
@@ -19,3 +19,17 @@ Then(/^I should be able to mark the checkbox$/) do
     find('#sortable > tbody > tr:nth-child(1) > td:nth-child(8) > input')[:value]
     ).to eq("true") 
 end   
+
+Then(/^I should be able to fill a comment in table objetivo$/) do
+  comment = 'a'*151
+  
+  find('#tabla_objetivos > tbody > tr:nth-child(1) > td:nth-child(7) > textarea', visible: false).set(comment)
+
+  click_link('Mi Agile Roadmap+')
+ 
+  # Need this for loading all the rows
+  sleep 5
+  
+  expect(find('#tabla_objetivos > tbody > tr:nth-child(1) > td:nth-child(7) > textarea', visible: false).text).to eq(comment)
+end
+
