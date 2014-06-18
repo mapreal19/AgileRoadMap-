cbbf63e8ee198f00851d520f20872cce8a9ac87b
Mario Perez <mapreal19@gmail.com>
2014-06-18 12:26:35 +0200

    Added migration for no_interesa column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140424104457) do
+ActiveRecord::Schema.define(version: 20140618102226) do
 
   create_table "ambito_trabajos", force: true do |t|
     t.string   "nombre"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 20140424104457) do
     t.integer  "position"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "no_interesa", default: false
   end
 
   add_index "user_objetivos", ["objetivo_id"], name: "index_user_objetivos_on_objetivo_id"
