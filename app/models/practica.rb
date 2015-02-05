5e69bd80fa760d58fe86944fb4824f9f686de3a9
Mario Perez <mapreal19@gmail.com>
2015-02-05 20:33:14 +0100

    Practica name getter depending on the locale

--- app/models/practica.rb
+++ app/models/practica.rb
@@ -1,36 +1,37 @@
 class Practica < ActiveRecord::Base
-	default_scope { order('position') }
+  default_scope { order('position') }
 
-	has_many :contribucions
-	has_many :objetivos, through: :contribucions
+  has_many :contribucions
+  has_many :objetivos, through: :contribucions
 
-	has_many :practica_desafios
-	has_many :desafios, through: :practica_desafios
+  has_many :practica_desafios
+  has_many :desafios, through: :practica_desafios
 
-	ESFUERZO = { 1 => 'Muy poco', 2 =>'Bajo', 3 => 'Medio', 4 => 'Alto', 5 => 'Muy alto'}
+  ESFUERZO = { 1 => 'Muy poco', 2 =>'Bajo', 3 => 'Medio', 4 => 'Alto', 5 => 'Muy alto'}
 
-	has_many :relacion_practicas
-	has_many :practicas, through: :relacion_practicas
-	# Other side of the relation
-	has_many :inverse_relacion_practicas, class_name: "RelacionPractica", foreign_key: 'practica2_id'
-	has_many :inverse_practicas, through: :inverse_relacion_practicas, source: :practica
+  has_many :relacion_practicas
+  has_many :practicas, through: :relacion_practicas
+  # Other side of the relation
+  has_many :inverse_relacion_practicas, class_name: "RelacionPractica", foreign_key: 'practica2_id'
+  has_many :inverse_practicas, through: :inverse_relacion_practicas, source: :practica
 
-	def get_json_relacion_practicas_ids
-		arr1 = self.relacion_practicas.pluck(:practica2_id)
-		arr2 = self.inverse_relacion_practicas.pluck(:practica_id)
-		ActiveSupport::JSON.encode(arr1 + arr2)
+  def get_json_relacion_practicas_ids
+    arr1 = self.relacion_practicas.pluck(:practica2_id)
+    arr2 = self.inverse_relacion_practicas.pluck(:practica_id)
+    ActiveSupport::JSON.encode(arr1 + arr2)
+  end
+
+  ##
+  # Gets the name of the practica taking into account the locale
+  def name
+    self["name_#{I18n.locale}"]
   end
 
   def position_with_prefix
-    "PRA" + self[:position].to_s
+    "PRA#{self[:position]}"
   end
   
   def position_with_prefix_and_name
-    if I18n.locale == :es 
-      position_with_prefix + ' ' + self[:name_es]
-    else
-      position_with_prefix + ' ' + self[:name_en]
-    end
+    "#{position_with_prefix} #{name}"
   end
-
 end
