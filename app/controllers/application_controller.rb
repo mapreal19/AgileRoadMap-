3dd7f872f84c16f9985b899a6aa7551d02e44482
Mario Perez <mapreal19@gmail.com>
2019-02-04 18:08:23 +0100

    fixes indent in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,11 +13,10 @@ class ApplicationController < ActionController::Base
 
   private
 
-  # http://xyzpub.com/en/ruby-on-rails/3.2/i18n_mehrsprachige_rails_applikation.html
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
     Rails.application.routes.default_url_options[:locale] = I18n.locale
-   end
+  end
 
   def allow_iframe_requests
     response.headers.delete('X-Frame-Options')
