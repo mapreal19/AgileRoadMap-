584d386f1a9feb0c63dd51938488d28a4b44a0be
Mario Perez <mapreal19@gmail.com>
2019-02-04 11:16:43 +0100

    Refactor test

--- spec/system/user_can_change_sort_order_spec.rb
+++ spec/system/user_can_change_sort_order_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+# NOTE: using Selenium for drag_to
 RSpec.describe 'User can change sort order', type: :system, selenium: true do
   it 'successfully' do
     user = create(:user, password: 'password')
@@ -7,17 +8,21 @@ RSpec.describe 'User can change sort order', type: :system, selenium: true do
     practica2 = create(:user_practica, user: user, position: 2)
     practica3 = create(:user_practica, user: user, position: 3)
 
-    visit signin_path
-    fill_in 'session[email]', with: user.email
-    fill_in 'session[password]', with: 'password'
-    within 'form' do
-      click_on 'Login'
-    end
+    login(email: user.email, password: 'password')
     practica1_handler = page.find("tr[data-practica-id='#{practica1.id}'] td.handle")
     practica3_handler = page.find("tr[data-practica-id='#{practica3.id}'] td.handle")
     practica3_handler.drag_to practica1_handler
     visit current_path
 
     expect(practica3.name_es).to appear_before(practica2.name_es)
+  end
+
+  def login(email:, password:)
+    visit signin_path
+    fill_in 'session[email]', with: email
+    fill_in 'session[password]', with: password
+    within 'form' do
+      click_on 'Login'
+    end
   end
 end
